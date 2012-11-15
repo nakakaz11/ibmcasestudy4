14248db9552c8eb800a6e3006b40e37bbd9fd260
Kaz Nakamura <nakakaz11@gmail.com>
2012-11-15 14:48:56 +0900

    fix1115

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 
 exports.index = function(request, response){
   response.render('index', {
-    title: 'SW (node.js+express IBM cs4)'
+    title: 'SW (node.js+express IBM cs4)use ejs+coffee'
   , desc: 'Twitter&Facebook searchTest' });
 
 };
